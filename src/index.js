ef64d33c0f6546577bcceec889d72ff6ad92dc3c
eric-j-owen <145154417+eric-j-owen@users.noreply.github.com>
2024-03-02 11:06:06 -0500

    adds basic rendering to the browser

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import createProject from './projects';
 import createTodo from './todos';
 import render from './render';
-import { getAllProjects, addProjectToStorage } from './storage';
+import { addProjectToStorage } from './storage';
 
-const testProject = createProject('project1', 'this is a test project');
-addProjectToStorage(testProject);
+const testProject1 = createProject('project1', 'this is a test project');
+const testProject2 = createProject('project2', 'this is ANOTHER test project');
+addProjectToStorage(testProject1);
+addProjectToStorage(testProject2);
 
 const testTodo = createTodo(
   'todo 1',
   'first todo i gotta do',
-  testProject.id,
   new Date(2024, 2, 1),
   1,
 );
 const testTodo2 = createTodo(
   'todo 2',
   'second todo i gotta do',
-  testProject.id,
   new Date(2024, 4, 15),
   2,
 );
@@ -26,9 +26,17 @@ const testTodo3 = createTodo(
   new Date(2025, 1, 20),
   3,
 );
-testProject.addTodo(testTodo);
-testProject.addTodo(testTodo2);
-testProject.addTodo(testTodo3);
+const testTodo4 = createTodo(
+  'todo 4',
+  'fourth todo i gotta do',
+  new Date(2025, 1, 20),
+  3,
+);
 
-testProject.removeTodo(testTodo2);
-render(getAllProjects());
+testProject1.addTodo(testTodo);
+testProject1.addTodo(testTodo2);
+testProject1.addTodo(testTodo3);
+
+testProject2.addTodo(testTodo4);
+
+render();
--- src/render.js
+++ src/render.js
@@ -1,3 +1,35 @@
-export default function render(obj) {
-  console.log(obj);
+import { getAllProjects } from './storage';
+
+export default function render() {
+  const projects = getAllProjects();
+  const projectsDiv = document.querySelector('.projects');
+  
+  projects.forEach((proj) => {
+    const projectDivEl = document.createElement('div');
+    projectDivEl.classList.add('project');
+    const projectTitleEl = document.createElement('h4');
+    const projectDescEl = document.createElement('p');
+    const todosDiv = document.createElement('div');
+
+    projectTitleEl.textContent = proj.title;
+    projectDescEl.textContent = proj.desc;
+
+    proj.todos.forEach((todo) => {
+      const todoDiv = document.createElement('div');
+      todoDiv.classList.add('todo')
+      const todoTitleEl = document.createElement('h5');
+      todoTitleEl.textContent = todo.title;
+      todoDiv.appendChild(todoTitleEl);
+
+      todosDiv.appendChild(todoDiv);
+    });
+
+    projectDivEl.appendChild(projectTitleEl);
+    projectDivEl.appendChild(projectDescEl);
+    projectDivEl.appendChild(todosDiv);
+
+    projectDivEl.append(document.createElement('hr'));
+    console.log(projectDivEl);
+    projectsDiv.appendChild(projectDivEl);
+  });
 }
